3be0ad26ee93258565cb466435d0e2fdcd2e69b9
Prithwis Das <72024767+Prithwis-2023@users.noreply.github.com>
2021-07-08 20:55:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-import tkinter as tk
-root=tk.Tk()
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-
-canvas1.pack()
-def hello ():
-    label1 = tk.Label(root, text= 'Hello, Welcome To PyCalc! Now you can use the application. Click the cross Button to open the calculator.', fg='green', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
-button1 = tk.Button(text='CLICK TO START',command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
-
-
-root.mainloop()
-#Start of code and action
 def ADD(A,B):
     return A+B
 def SUBSTRACT(A,B):
